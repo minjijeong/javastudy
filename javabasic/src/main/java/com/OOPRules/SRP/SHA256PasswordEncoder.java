ae2b7e482ab08f871d51a7ac8b9e7ce5c7efe4d1
Minji Jeong <minji279@gmail.com>
2022-07-27 10:59:46 +0900

    [STUDY][OOP] 예제 - OCP 개방 폐쇄 원칙에 위배

---
+++ javabasic/src/main/java/com/OOPRules/SRP/SHA256PasswordEncoder.java
@@ -0,0 +1,36 @@
+package com.OOPRules.SRP;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SHA256PasswordEncoder {
+    private final static String SHA_256 = "SHA-256";
+
+    public String encryptPassword(final String pw){
+        final MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance(SHA_256);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException();
+        }
+
+        final byte[] encodeHash = digest.digest(pw.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(encodeHash);
+    }
+
+    private String bytesToHex(byte[] encodeHash) {
+        final StringBuilder hexString = new StringBuilder(2 * encodeHash.length);
+
+        for(final byte hash : encodeHash){
+            final String hex = Integer.toHexString(0xff & hash);
+            if(hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
--- javabasic/src/main/java/com/OOPRules/SRP/UserService.java
+++ javabasic/src/main/java/com/OOPRules/SRP/UserService.java
@@ -9,7 +9,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final SimplePasswordEncoder passwordEncoder;
+    /**
+     * Encoder가 바뀜에 따라 기존 UserService의 소스가 변경되어야 하는 것은
+     * 개방폐쇄원칙에서 수정에 대해서는 닫혀있어야 한다는 원칙을 위배
+     */
+    private final SHA256PasswordEncoder passwordEncoder;
 
     public void addUser(final String email, final String pw) {
         // 비밀번호 암호화 처리 분리

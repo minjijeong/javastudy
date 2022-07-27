39d32777f99752bfaa7b7cbd05f0ee1c8161b2c3
Minji Jeong <minji279@gmail.com>
2022-07-27 10:52:50 +0900

    [STUDY][디자인패턴] 싱글톤 학습 및 예제 실습 2

---
+++ javabasic/src/main/java/com/designpattern/singleton/App.java
@@ -0,0 +1,23 @@
+package com.designpattern.singleton;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+//        EagerSingleton singleton = EagerSingleton.getInstance();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                StaticInnerSingleton singleton = StaticInnerSingleton.getInstance();
+                System.out.println("Sync-> " + Thread.currentThread().getName() + ":"+ singleton.hashCode());
+            }).start();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                StaticInnerSingleton singleton = StaticInnerSingleton.getInstance();
+                System.out.println("Sync-> " + Thread.currentThread().getName() + ":"+ singleton.hashCode());
+            }).start();
+        }
+    }
+}
